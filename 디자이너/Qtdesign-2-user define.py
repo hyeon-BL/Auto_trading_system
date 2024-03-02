e67e1b5bebbf3c55d634d28080c4ad660b2ad383
Lee Hyeon Jun <lhbj1115@gmail.com>
2024-03-02 20:42:34 +0900

    버튼이 전부 눌리도록 수정(간격이 좁아서 그랬음)

--- 디자이너/Qtdesign-2-user define.py
+++ 디자이너/Qtdesign-2-user define.py
@@ -5,13 +5,14 @@ from PyQt5 import uic
 class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        w0 = Mywidgetset(self)
 
-        w1 = Mywidgetset(self)
-        w1.move(0, 40)
+        self.w0 = Mywidgetset("BTC",self)
 
-        # w2 = Mywidgetset(self)
-        # w2.move(0, 80)
+        self.w1 = Mywidgetset("LTC",self)
+        self.w1.move(0, 60)
+
+        self.w2 = Mywidgetset("XRP",self)
+        self.w2.move(0, 120)
 
 if __name__ == "__main__":
     app = QApplication([])
--- 디자이너/mywidget.py
+++ 디자이너/mywidget.py
@@ -4,11 +4,12 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QWidget
 
 class Mywidgetset(QWidget):
-    def __init__(self, parent = None):
+    def __init__(self, ticker, parent = None):
         super().__init__(parent)
         uic.loadUi('C:/Users/i4u11/Desktop/Work/Auto_trading_system/디자이너/mywidget.ui', self)
 
+        self.ticker = ticker
         self.btn.clicked.connect(self.btn_clicked)
 
     def btn_clicked(self):
-        print("버튼클릭")
+        print(f"{self.ticker} 버튼클릭")
