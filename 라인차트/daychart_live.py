526231aff6e11e5adb5f281b4d847b534fc61245
Lee Hyeon Jun <lhbj1115@gmail.com>
2024-04-21 21:03:55 +0900

    live chart

--- 라인차트/daychart_live.py
+++ 라인차트/daychart_live.py
@@ -3,7 +3,17 @@ from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from PyQt5.QtChart import *
 from PyQt5.QtGui import *
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import *
+import time
+
+class Worker(QThread):
+    dataSent = pyqtSignal(float)
+    def run(self):
+        while True:
+            df = pyupbit.get_current_price("KRW-BTC")
+            self.dataSent.emit(df)
+            time.sleep(1)
+
 
 class ChartWidget(QWidget):
     def __init__(self):
@@ -12,11 +22,6 @@ class ChartWidget(QWidget):
 
         # 1) 데이터 추가
         self.chartData = QLineSeries()
-        df = pyupbit.get_ohlcv("KRW-BTC", interval="day")
-        print(df.loc["2023", "close"])
-
-        for i in range(len(df.loc["2023","close"])):
-            self.chartData.append(i, df.loc["2023", "close"][i])
 
         # 2) 도화지 연결
         self.lineChart = QChart()
@@ -25,20 +30,46 @@ class ChartWidget(QWidget):
         self.lineChart.layout().setContentsMargins(0, 0, 0, 0)
 
         ax = QValueAxis()
-        ax.setRange(0, len(df.loc["2023","close"]))
         self.lineChart.addAxis(ax, Qt.AlignBottom)
-
 
         data = self.chartData.pointsVector()
         dataY = [p.y() for p in data]
 
         ay = QValueAxis()
-        ay.setRange(min(dataY),max(dataY))
         self.lineChart.addAxis(ay, Qt.AlignLeft)
+
+        self.chartData.attachAxis(ax)
+        self.chartData.attachAxis(ay) 
 
         # 3) 위젯에 출력
         self.priceview.setChart(self.lineChart)
         self.priceview.setRenderHint(QPainter.Antialiasing)
+
+
+        self.worker = Worker()
+        self.worker.dataSent.connect(self.updateData)
+        self.worker.start()
+
+        self.idx = 0
+        self.viewlimit = 30
+
+    def updateData(self, price):
+        if self.idx > self.viewlimit:
+            self.chartData.remove(0)
+
+        self.chartData.append(self.idx, price)
+        self.idx += 1
+
+        pvs = self.chartData.pointsVector()
+        x = pvs[0].x()
+
+        ax = self.lineChart.axisX()
+        ax.setRange(x, x + self.viewlimit - 1)
+
+        ay = self.lineChart.axisY()
+        dataY = [p.y() for p in pvs]
+        ay.setRange(min(dataY), max(dataY))
+
 
 if __name__ == "__main__":
     app = QApplication([])
