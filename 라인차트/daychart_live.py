fa29ed39d407e5afd291c3f75c4000ce00feab4d
Lee Hyeon Jun <lhbj1115@gmail.com>
2024-04-25 11:16:07 +0900

    realtime chart axis revision

--- 라인차트/daychart_live.py
+++ 라인차트/daychart_live.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from PyQt5.QtChart import *
 from PyQt5.QtGui import *
-from PyQt5.QtCore import *
+from PyQt5.QtCore import QDateTime, Qt, QThread, pyqtSignal
 import time
 
 class Worker(QThread):
@@ -29,7 +29,9 @@ class ChartWidget(QWidget):
         self.lineChart.legend().hide()
         self.lineChart.layout().setContentsMargins(0, 0, 0, 0)
 
-        ax = QValueAxis()
+        ax = QDateTimeAxis()
+        ax.setFormat("hh:mm:ss")
+        ax.setTickCount(4)
         self.lineChart.addAxis(ax, Qt.AlignBottom)
 
         data = self.chartData.pointsVector()
@@ -50,25 +52,32 @@ class ChartWidget(QWidget):
         self.worker.dataSent.connect(self.updateData)
         self.worker.start()
 
-        self.idx = 0
         self.viewlimit = 30
 
     def updateData(self, price):
-        if self.idx > self.viewlimit:
+        if len(self.chartData) > self.viewlimit:
             self.chartData.remove(0)
 
-        self.chartData.append(self.idx, price)
-        self.idx += 1
+        curr = QDateTime.currentDateTime()
+        self.chartData.append(curr.toMSecsSinceEpoch(), price)
 
         pvs = self.chartData.pointsVector()
         x = pvs[0].x()
 
+        s = QDateTime.fromMSecsSinceEpoch(int(x))
+        e = s.addSecs(self.viewlimit)
+
         ax = self.lineChart.axisX()
-        ax.setRange(x, x + self.viewlimit - 1)
+        ax.setRange(s, e)
 
         ay = self.lineChart.axisY()
         dataY = [p.y() for p in pvs]
-        ay.setRange(min(dataY), max(dataY))
+
+        minval = min(dataY)
+        maxval = max(dataY)
+        margin = (maxval - minval) * 0.2
+
+        ay.setRange(minval - margin, maxval + margin)
 
 
 if __name__ == "__main__":
