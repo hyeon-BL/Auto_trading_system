49ab1b79f11b58578f2a8a01fb7fe5eb6fcfbf40
Lee Hyeon Jun <lhbj1115@gmail.com>
2024-06-14 17:02:28 +0900

    Refactor public offering calendar code

--- Calender/public offering.py
+++ Calender/public offering.py
@@ -9,20 +9,14 @@ import re
 
 KST = timezone('Asia/Seoul')
 today = datetime.datetime.now(KST)
+print(today)
 
-def isDateInRange(created_at):
-    suffix_KST = '.000001+09:00'
-    created_at = parse(created_at + suffix_KST)
-    yesterday = today - datetime.timedelta(1)
-    return today > created_at and created_at > yesterday
 
 site = 'http://www.ipostock.co.kr'
 res = urlopen(site + '/sub03/ipo06.asp')
 soup = BeautifulSoup(res, 'html.parser')
 day = soup.select('.days')
-print(day)
 date = [d.select('strong') for d in day]
-print(date)
 
 
 def search_stock(s):
@@ -32,35 +26,23 @@ def search_stock(s):
     return category[int(stock[-1])]
 
 
-calender = [[], []]
+calender = {}
 for d in date:
     try:
-        print(d[0].get_text(), '일', '공모주')
+        t = d[0].get_text()
+        calender[t] = [[], []]
+        print(t, '일', '공모주')
         stock = day[date.index(d)].select('a')
         img = day[date.index(d)].select('img')
         for i in img:
-            calender[0].append(search_stock(i))
+            calender[t][0].append(search_stock(i))
         for s in stock:
-            calender[1].append(s.get('title'))
+            calender[t][1].append(s.get('title'))
     except:
         print('공모주 없음')
-print(calender, len(calender[0]), len(calender[1]))
-# issue_body = ''
+print(calender)
 
-# for row in article_list:
-#     title = row.select('h5 > a')[0]
-#     published_at = row.select('div.date-created span.timeago')[0].get_text()
-#     item = published_at + " " +  str(title).replace('href="','href="' + site).replace("\n", "").replace('  ', '').strip() + '<br/>\n'
-#     if '마감' not in str(title) and isDateInRange(published_at):
-#         issue_body += item
-#     else: 
-#         print('[filtered] ', item)
 
-# print('----------------------------------------------------------------------')
-
-# issue_title = "스터디 모집 글 모음(%s)" % (today.strftime("%Y년 %m월 %d일 %H시"))
-# print(issue_title)
-# print(issue_body)
 
 # GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
 # REPO_NAME = "crawler-study-gathering"
